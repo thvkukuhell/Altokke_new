ab7eda2a60f3d7dec018fedada43d9b002e0e0ee
Donina <donigarro@gmail.com>
2026-07-10 22:22:32 -0500

    Hice que el correo de resumen de viaje se envie en segundo plano para no trabar la peticion

--- app/Mail/ViajeCompletadoMail.php
+++ app/Mail/ViajeCompletadoMail.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Models\Viaje;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ViajeCompletadoMail extends Mailable
+class ViajeCompletadoMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
